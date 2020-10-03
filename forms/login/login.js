83317af8377ad27f8031b10445f43117d969de49
Yidedia <70745425+Yidedia@users.noreply.github.com>
2020-10-03 11:32:29 -0500

    fsda

--- forms/login/login.js
+++ forms/login/login.js
@@ -11,12 +11,6 @@ function homeLoanPayment(p,r,n) {
 carLoan.onclick=function(){
   a = Number(principle.value)
   r = Number(rate.value)
-<<<<<<< HEAD
-  n = Number(months.value)
-  amount = carLoanPayment(a,r,n)
-  answer.value = `A car loan for $${a} over ${n} months at ${r}% interest would have a monthly payment of $${amount}` 
-}
-=======
   n = Number(years.value) * 12
   amount = carLoanPayment(a,r,n)
   answer.value = `A car loan for $${a} over ${n} months at ${r}% interest would have a monthly payment of $${amount}` 
@@ -35,4 +29,3 @@ Clear.onclick=function(){
   rate.value = ''
   years.value = ''
 }
->>>>>>> home-loan
